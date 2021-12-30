a8a568c4fb6cd06098418efca7addebb3d387840
amaking7 <84038322+amaking7@users.noreply.github.com>
2021-12-29 20:22:10 -0500

    Added temperature conversion

--- src/Temperature.js
+++ src/Temperature.js
@@ -1,21 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
 
 export default function Temperature(props) {
-  return (
-    <div className="Temperature">
-      <span className="current-city-temperature" id="current-temperature">
-        {props.celsiusTemp}
-      </span>
-      <span className="weather-units">
-        <a href="_" id="celsius-link" class="active">
-          °C
-        </a>{" "}
-        |
-        <a href="_" id="fahrenheit-link">
-          °F
-        </a>
-      </span>
-    </div>
-  );
+  const [unit, setUnit] = useState("celsius");
+
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  function fahrenheit() {
+    return (props.celsiusTemp * 9) / 5 + 32;
+  }
+
+  if (unit === "celsius") {
+    return (
+      <div className="Temperature">
+        <span className="current-city-temperature" id="current-temperature">
+          {props.celsiusTemp}
+        </span>
+        <span className="weather-units">
+          °C |
+          <a href="/" id="fahrenheit-link" onClick={showFahrenheit}>
+            {"  "} °F
+          </a>
+        </span>
+      </div>
+    );
+  } else {
+    return (
+      <div className="Temperature">
+        <span className="current-city-temperature" id="current-temperature">
+          {Math.round(fahrenheit())}
+        </span>
+        <span className="weather-units">
+          <a href="/" onClick={showCelsius}>
+            °C
+          </a>{" "}
+          |{"  "}°F
+        </span>
+      </div>
+    );
+  }
 }
